73fa7f15c06bbe838d9732814e3b3c935e5720d1
NiRzaRr <101584926+NiRzaRr@users.noreply.github.com>
2022-07-22 07:14:55 +0530

    Add files via upload

---
+++ static website(Music Studio)/index.js
@@ -0,0 +1,9 @@
+  var listt = document.getElementById("mob_items");
+ function home_list(e){
+  if (listt.style.display != "block"){
+      listt.style.display = "block";
+    }
+    else{
+      listt.style.display = "none";
+    }
+  }
